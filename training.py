b711f4894cdf4bc1c0b9c93cad51c7ca7699fc56
Can Pouliquen <can.pouliquen@ens-lyon.fr>
2025-10-31 14:14:08 +0100

    add training config, fix minor issues

--- training.py
+++ training.py
@@ -7,17 +7,17 @@ import torch
 import numpy as np
 from pathlib import Path
 
-from args_parser import parse_args
 from transformer import TransformerLM
 from optimizers import AdamW
 from losses import CrossEntropyLoss, Perplexity
 from training_utils import get_batch, load_checkpoint, save_checkpoint
+from optim_utils import cosine_annealing_scheduler, gradient_clipping
+from training_configs import Config
 
 torch.manual_seed(0)
 
-# TODO Use either default config or user-specified setting
 # TODO set-up Wandb
-args = parse_args()
+args = Config()
 
 device = args.device
 
@@ -26,10 +26,10 @@ train_data = "./data_tokenized/TinyStoriesV2-GPT4-train_token_ids_bis.npy"
 valid_data = "./data_tokenized/TinyStoriesV2-GPT4-valid_token_ids_bis.npy"
 
 # Training data contains approx. 540M tokens
-train_token_ids = np.memmap(train_data, dtype=np.uint16, mode="r")
+train_token_ids = np.load(train_data, mmap_mode='r')
 
 # Validation data contains approx. 5.5M tokens
-valid_token_ids = np.memmap(valid_data, dtype=np.uint16, mode="r")
+valid_token_ids = np.load(valid_data, mmap_mode='r')
 
 # Instantiate the model
 model = TransformerLM(vocab_size=args.vocab_size,
@@ -52,16 +52,14 @@ perplexity = Perplexity()
 checkpoint_path = Path(args.save_checkpoints_to)
 checkpoint_path.mkdir(parents=True, exist_ok=True)
 
-# TODO make these a user-specified argument ?
-total_tokens_processed = 327_680_000 # For GPU
-# total_tokens_processed = 40_960_000  # For CPU
-
-total_step_count = 10_000
+# Set-up training hyperparams
+total_tokens_processed = args.total_tokens_processed
+total_step_count = args.total_step_count
 context_length = args.context_length
+
 batch_size = int(total_tokens_processed / (total_step_count * context_length))
 
-# TODO scheduler
-# TODO gradient clipping
+# TODO gradient clipping ?
 
 # Resume training if specified
 epoch = -1
@@ -74,9 +72,19 @@ model.to(device)
 while epoch < total_step_count:
     epoch += 1
     print(f"Epoch {epoch} / {total_step_count}")
-    model.train()
 
+    model.train()
     optimizer.zero_grad()
+
+    if args.lr_scheduler:
+        learning_rate = cosine_annealing_scheduler(iteration_idx=epoch,
+                                                    maximal_learning_rate=args.adamw_lr,
+                                                    minimal_learning_rate=args.minimal_learning_rate_ratio*args.adamw_lr,
+                                                    nb_warmup_iters=int(args.nb_warmup_iters_ratio*total_step_count),
+                                                    nb_cosine_annealing_iters=args.nb_cosine_annealing_iters)
+        for group in optimizer.param_groups:
+            group["lr"] = learning_rate
+
 
     inputs, targets = get_batch(train_token_ids,
                                 batch_size,
@@ -104,7 +112,7 @@ while epoch < total_step_count:
         loss = criterion(outputs, targets)
         print(f"Valid loss: {loss.item():.3f}")
 
-    # Checkpoint the run every 10 epochs
+    # Checkpoint the run every 100 epochs
     saving_location = checkpoint_path / Path(f"epoch_{epoch}.pt")
     if epoch % 100 == 0:
         save_checkpoint(model=model,
---
+++ training_configs.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+
+    # Transformer hyperparameters
+    resume_training : bool = False
+    vocab_size: int = 10_000
+    context_length: int = 256
+    num_layers: int = 4
+    d_model: int = 512
+    num_heads: int = 16
+    d_ff:int = 1344
+    theta: int = 10000 # RoPE Theta parameter
+
+    # Training hyperparameters
+    total_tokens_processed: int = 327_680_000
+    total_step_count: int = 10_000
+    batch_size: int = int(total_tokens_processed / (total_step_count * context_length)) # Currently yields 128
+
+    # AdamW hyperparameters
+    adamw_lr: float = 1e-3
+    device: str = "cuda:0"
+    save_checkpoints_to: str = "./checkpoints/"
+
+    # Learning rate scheduling parameterss
+    lr_scheduler: bool = True
+    maximal_learning_rate: float = adamw_lr
+    minimal_learning_rate_ratio: float = 0.1
+    nb_warmup_iters_ratio: float = 0.01
+    nb_cosine_annealing_iters: int = int(total_step_count - nb_warmup_iters_ratio*total_step_count)
